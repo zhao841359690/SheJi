510f73e256e492714bcc9215bbedc537676bf2db
guobaocheng <bguo@xumo.com>
2019-09-16 17:31:14 +0800

    测试串连数据

--- app/src/main/java/com/sheji/sheji/util/SerialPortUtils.java
+++ app/src/main/java/com/sheji/sheji/util/SerialPortUtils.java
@@ -1,10 +1,15 @@
 package com.sheji.sheji.util;
 
+import android.os.Bundle;
 import android.util.Log;
+
+import com.sheji.sheji.activity.MainActivity;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
 import android_serialport_api.SerialPort;
 
 /**
@@ -12,118 +17,103 @@ import android_serialport_api.SerialPort;
  */
 
 public class SerialPortUtils {
+    private String port = "ttyS1";//串口号
+    private int baudrate = 9600;//波特率
 
-    private final String TAG = "SerialPortUtils";
-    private String path = "/dev/ttyS1";
-    private int baudrate = 9600;
-    public boolean serialPortStatus = false; //是否打开串口标志
-    public String data_;
-    public boolean threadStatus; //线程状态，为了安全终止线程
-
-    public SerialPort serialPort = null;
-    public InputStream inputStream = null;
-    public OutputStream outputStream = null;
-    public ChangeTool changeTool = new ChangeTool();
-
+    public static SerialPort serialPort = null;
+    public static InputStream inputStream = null;
+    public static OutputStream outputStream = null;
+    public static Thread receiveThread = null;
+    public static boolean flag = false;
 
     /**
      * 打开串口
+     *
      * @return serialPort串口对象
      */
-    public SerialPort openSerialPort(){
+    public SerialPort openSerialPort() {
         try {
-                serialPort = new SerialPort(new File(path),baudrate,0);
-                this.serialPortStatus = true;
-                threadStatus = false; //线程状态
-
-                //获取打开的串口中的输入输出流，以便于串口数据的收发
-                inputStream = serialPort.getInputStream();
-                outputStream = serialPort.getOutputStream();
-
-                new ReadThread().start(); //开始线程监控是否有数据要接收
+            serialPort = new SerialPort(new File("/dev/" + port), baudrate, 0);
+            //获取打开的串口中的输入输出流，以便于串口数据的收发
+            inputStream = serialPort.getInputStream();
+            outputStream = serialPort.getOutputStream();
+            flag = true;
+            receiveSerialPort();
         } catch (IOException e) {
-            Log.e(TAG, "openSerialPort: 打开串口异常：" + e.toString());
             return serialPort;
         }
-        Log.d(TAG, "openSerialPort: 打开串口");
         return serialPort;
     }
 
     /**
-     * 关闭串口
+     * 关闭串口的方法
+     * 关闭串口中的输入输出流
      */
-    public void closeSerialPort(){
+    public static void closeSerialPort() {
         try {
-            inputStream.close();
-            outputStream.close();
-
-            this.serialPortStatus = false;
-            this.threadStatus = true; //线程状态
-            serialPort.close();
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            if (serialPort != null) {
+                serialPort.close();
+            }
+            flag = false;
         } catch (IOException e) {
-            Log.e(TAG, "closeSerialPort: 关闭串口异常："+e.toString());
-            return;
+            e.printStackTrace();
         }
-        Log.d(TAG, "closeSerialPort: 关闭串口成功");
     }
 
     /**
-     * 发送串口指令（字符串）
-     * @param data String数据指令
+     * 发送串口数据的方法
+     *
+     * @param data 要发送的数据
      */
-    public void sendSerialPort(String data){
-        Log.d(TAG, "sendSerialPort: 发送数据");
-
+    public void sendSerialPort(String data) {
         try {
-            byte[] sendData = data.getBytes(); //string转byte[]
-            this.data_ = new String(sendData); //byte[]转string
+            byte[] sendData = data.getBytes();
             if (sendData.length > 0) {
                 outputStream.write(sendData);
                 outputStream.write('\n');
-                //outputStream.write('\r'+'\n');
                 outputStream.flush();
-                Log.d(TAG, "sendSerialPort: 串口数据发送成功");
             }
         } catch (IOException e) {
-            Log.e(TAG, "sendSerialPort: 串口数据发送失败："+e.toString());
+            e.printStackTrace();
         }
 
     }
 
     /**
-     * 单开一线程，来读数据
+     * 接收串口数据的方法
      */
-    private class ReadThread extends Thread{
-        @Override
-        public void run() {
-            super.run();
-            //判断进程是否在运行，更安全的结束进程
-            while (!threadStatus){
-                Log.d(TAG, "进入线程run");
-                //64   1024
-                byte[] buffer = new byte[64];
-                int size; //读取数据的大小
-                try {
-                    size = inputStream.read(buffer);
-                    if (size > 0){
-                        Log.d(TAG, "run: 接收到了数据：" + changeTool.ByteArrToHex(buffer));
-                        Log.d(TAG, "run: 接收到了数据大小：" + String.valueOf(size));
-                        onDataReceiveListener.onDataReceive(buffer,size);
+    public static void receiveSerialPort() {
+        if (receiveThread != null) {
+            return;
+        }
+        /*创建子线程接收串口数据
+         */
+        receiveThread = new Thread() {
+            @Override
+            public void run() {
+                while (flag) {
+                    try {
+                        byte[] readData = new byte[1024];
+                        if (inputStream == null) {
+                            return;
+                        }
+                        int size = inputStream.read(readData);
+                        if (size > 0 && flag) {
+                            String recinfo = new String(readData, 0, size);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    Log.e(TAG, "run: 数据读取异常：" +e.toString());
                 }
             }
-
-        }
+        };
+        //启动接收线程
+        receiveThread.start();
     }
-
-    public OnDataReceiveListener onDataReceiveListener = null;
-    public static interface OnDataReceiveListener {
-        public void onDataReceive(byte[] buffer, int size);
-    }
-    public void setOnDataReceiveListener(OnDataReceiveListener dataReceiveListener) {
-        onDataReceiveListener = dataReceiveListener;
-    }
-
 }
