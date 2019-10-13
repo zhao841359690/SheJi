acb9ef7dbe76a7edf8f8cfceecd7d0843cb0d7c4
Administrator <841359690@qq.com>
2019-10-13 09:55:43 +0800

    发送接收数据为16进制数据

--- app/src/main/java/com/sheji/sheji/activity/LoginActivity.java
+++ app/src/main/java/com/sheji/sheji/activity/LoginActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
