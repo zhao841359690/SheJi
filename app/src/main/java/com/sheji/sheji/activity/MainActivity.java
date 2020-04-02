25ec22fa484b1ebf989ed3d7d0968a438961cdd5
kk-zhaoqingfeng <klin@xumo.com>
2020-04-02 09:58:14 +0800

    4.开关的颜色统一一下

--- app/src/main/java/com/sheji/sheji/activity/MainActivity.java
+++ app/src/main/java/com/sheji/sheji/activity/MainActivity.java
@@ -644,35 +644,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
                     mIlluminationTv.setText("射击靶照明关");
                     mIlluminationSwitch.setChecked(false);
 
-                    //TODO 控制靶机照明(打开)
-                    //控制靶机照明(打开)
-                    if (SerialPortUtils.getInstance().openSerialPort() == null) {
-                        Toast.makeText(MainActivity.this, "设备打开异常,正在尝试重新打开设备", Toast.LENGTH_SHORT).show();
-                        SerialPortUtils.getInstance().openSerialPort();
-                    } else {
-                        byte[] sendByte = new byte[11];
-                        sendByte[0] = (byte) Integer.parseInt("CC", 16);
-                        sendByte[1] = (byte) Integer.parseInt("23", 16);
-                        sendByte[2] = (byte) Integer.parseInt("AA", 16);
-                        sendByte[3] = (byte) Integer.parseInt("DF", 16);
-
-                        int equipment = Integer.valueOf(SharedPreferencesUtils.getInstance().getEquipmentNumber());
-                        String e = Integer.toHexString(equipment);
-                        sendByte[4] = (byte) Integer.parseInt("00", 16);
-                        sendByte[5] = (byte) Integer.parseInt("00", 16);
-                        sendByte[6] = (byte) Integer.parseInt("00", 16);
-                        sendByte[7] = (byte) Integer.parseInt(e, 16);
-
-                        sendByte[8] = (byte) Integer.parseInt("01", 16);
-
-                        sendByte[9] = (byte) Integer.parseInt("0A", 16);
-                        sendByte[10] = (byte) Integer.parseInt("0D", 16);
-
-                        SerialPortUtils.getInstance().sendSerialPort(sendByte);
-                    }
-                } else {
-                    mIlluminationTv.setText("射击靶照明开");
-                    mIlluminationSwitch.setChecked(true);
                     //TODO 控制靶机照明(关闭)
                     //控制靶机照明(关闭)
                     if (SerialPortUtils.getInstance().openSerialPort() == null) {
@@ -693,6 +664,35 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
                         sendByte[7] = (byte) Integer.parseInt(e, 16);
 
                         sendByte[8] = (byte) Integer.parseInt("00", 16);
+
+                        sendByte[9] = (byte) Integer.parseInt("0A", 16);
+                        sendByte[10] = (byte) Integer.parseInt("0D", 16);
+
+                        SerialPortUtils.getInstance().sendSerialPort(sendByte);
+                    }
+                } else {
+                    mIlluminationTv.setText("射击靶照明开");
+                    mIlluminationSwitch.setChecked(true);
+                    //TODO 控制靶机照明(打开)
+                    //控制靶机照明(打开)
+                    if (SerialPortUtils.getInstance().openSerialPort() == null) {
+                        Toast.makeText(MainActivity.this, "设备打开异常,正在尝试重新打开设备", Toast.LENGTH_SHORT).show();
+                        SerialPortUtils.getInstance().openSerialPort();
+                    } else {
+                        byte[] sendByte = new byte[11];
+                        sendByte[0] = (byte) Integer.parseInt("CC", 16);
+                        sendByte[1] = (byte) Integer.parseInt("23", 16);
+                        sendByte[2] = (byte) Integer.parseInt("AA", 16);
+                        sendByte[3] = (byte) Integer.parseInt("DF", 16);
+
+                        int equipment = Integer.valueOf(SharedPreferencesUtils.getInstance().getEquipmentNumber());
+                        String e = Integer.toHexString(equipment);
+                        sendByte[4] = (byte) Integer.parseInt("00", 16);
+                        sendByte[5] = (byte) Integer.parseInt("00", 16);
+                        sendByte[6] = (byte) Integer.parseInt("00", 16);
+                        sendByte[7] = (byte) Integer.parseInt(e, 16);
+
+                        sendByte[8] = (byte) Integer.parseInt("01", 16);
 
                         sendByte[9] = (byte) Integer.parseInt("0A", 16);
                         sendByte[10] = (byte) Integer.parseInt("0D", 16);
